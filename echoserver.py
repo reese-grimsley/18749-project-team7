033309a4a8546b7ae71db6c9fd98538ed06ecbd7
Reese <rgrimsle@andrew.cmu.edu>
2021-09-20 13:40:03 -0400

    Milestone 1 - threadless and short connection with LFD

--- echoserver.py
+++ echoserver.py
@@ -3,6 +3,7 @@
 # used https://realpython.com/python-sockets/
 
 import socket
+import traceback
 import DebugLogger
 from lfd import MAGIC_MSG_LFD_REQUEST, MAGIC_MSG_LFD_RESPONSE
 
@@ -47,31 +48,44 @@ PORT = DEFAULT_PORT
             
 #             conn.sendall(data)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    state_x = 0
-    while True: 
-        conn, addr = s.accept()
-        with conn:
-            logger.info('Connected by %s', addr)
-            logger.info("state_x is " + str(state_x))
-            data = conn.recv(1024)
-            if not data:
-                #print("Waiting for something else")
-                continue
-            logger.info(data)
-            # server received valid data
-            # we now need to distinguish if this is from client or lfd
-            if data.decode('utf-8') == MAGIC_MSG_LFD_REQUEST:
-                # this is heartbeat so we won't increment
-                logger.info("received heartbeat from LFD")
-                response_data = MAGIC_MSG_LFD_RESPONSE
-                
-            else:
-                state_x += 1
-                logger.info("state_x after the increment is " + str(state_x))
-                response_data = data
-            
-            conn.sendall(data)
+try: 
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+            s.bind((HOST, PORT))
+            s.listen()
+            state_x = 0
+            while True: 
+                conn, addr = s.accept()
+                with conn:
+                    logger.info('Connected by %s', addr)
+                    logger.info("state_x is " + str(state_x))
+                    data = conn.recv(1024)
+                    if not data:
+                        #print("Waiting for something else")
+                        continue
+                    logger.info(data)
+                    # server received valid data
+                    # we now need to distinguish if this is from client or lfd
+                    if data.decode('utf-8') == MAGIC_MSG_LFD_REQUEST:
+                        # this is heartbeat so we won't increment
+                        logger.info("received heartbeat from LFD")
+                        response_data = MAGIC_MSG_LFD_RESPONSE
+                        
+                    else:
+                        state_x += 1
+                        logger.info("state_x after the increment is " + str(state_x))
+                        response_data = data
+                    
+                    conn.sendall(response_data)
+                    conn.close()
+        finally:
+            logger.debug('close main server socket')
+            s.close()
 
+except KeyboardInterrupt:
+    logger.critical('Keyboard interrupt in echoserver; exiting')
+except Exception as e:
+    logger.error('Caught exception: %s' % e)
+finally: 
+    logger.info("Echo Server Off.\n\n")
