7bbd855d5597f151ae5823b15701b97896838a70
Reese <rgrimsle@andrew.cmu.edu>
2021-09-20 13:56:28 -0400

    milestone 1

--- echoserver.py
+++ echoserver.py
@@ -11,47 +11,15 @@ logger = DebugLogger.get_logger('echoserver')
 
 LOCAL_HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 DEFAULT_PORT = 19618        # Port to listen on (non-privileged ports are > 1023)
-MAGIC_MSG_LFD = b"lfd-heartbeat" # if we receive this message, we know it's heartbeat, so x shouldn't be
-
 
 # host and port might change
 HOST = LOCAL_HOST
 PORT = DEFAULT_PORT
 
-# After successfully processing 1 heartbeat, server stops responding
-
-
-## This is the original server. I will be changing some structures
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.bind((HOST, PORT))
-#     s.listen()
-#     state_x = 0
-#     conn, addr = s.accept()
-#     with conn:
-#         logger.info('Connected by', addr)
-#         logger.info("state_x is " + str(state_x))
-#         while True:
-#             data = conn.recv(1024)
-#             if not data:
-#                 #print("Waiting for something else")
-#                 continue
-#             logger.info(data)
-#             # server received valid data
-#             # we now need to distinguish if this is from client or lfd
-#             if data == MAGIC_MSG_LFD:
-#                 # this is heartbeat so we won't increment
-#                 logger.info("received heartbeat from LFD")
-                
-#             else:
-#                 state_x += 1
-#                 logger.info("state_x after the increment is " + str(state_x))
-            
-#             conn.sendall(data)
-
 try: 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         try:
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) # More portable to use socket.SO_REUSEADDR than SO_REUSEPORT.
             s.bind((HOST, PORT))
             s.listen()
             state_x = 0
