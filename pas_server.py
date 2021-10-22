b315d2dfbd5e55e8222bf7725cbb6273745ff778
kirankumarhere <kiranx1kumar@gmail.com>
2021-10-22 18:14:53 -0400

    tried to implement flags for primary and backup

--- pas_server.py
+++ pas_server.py
@@ -19,14 +19,17 @@ PORT = constants.DEFAULT_APP_SERVER_PORT
 state_x = 0
 
 DebugLogger.set_console_level(30)
-logger = DebugLogger.get_logger('app_server')
+logger = DebugLogger.get_logger('passive_app_server')
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Application Server")
+    parser = argparse.ArgumentParser(description="Passive Application Server")
 
     parser.add_argument('-p', '--port', metavar='p', default=constants.DEFAULT_APP_SERVER_PORT, help='The port that the server will be listening to and that this LFD will access', type=int)
     parser.add_argument('-i', '--ip', metavar='i', default=constants.CATCH_ALL_IP, help='The IP address this server should bind to -- defaults to 0.0.0.0, which will work across any local address', type=str)
+
+    parser.add_argument('-f', '--flag', metavar='f', default=1, help='Primary is flag = 0 and Backup is flag = 1', type=int)
     args = parser.parse_args()
+
 
     if args.port < 1024 or args.port > 65535:
         raise ValueError('The port must be between 1024 and 65535')
@@ -34,39 +37,46 @@ def parse_args():
         print(args.ip)
         raise ValueError('The IP address given [%s] is not a valid format', args.ip)
 
-    return args.ip, args.port
+    if args.flag != 0 and args.flag != 1:
+        raise ValueError('Please enter a valid flag...')
 
-def application_server_handler(client_socket, client_addr):
+   
+
+
+    return args.ip, args.port, args.flag
+
+def passive_application_server_handler(client_socket, client_addr, flag):
     global state_x
     connected = True
     try:
         while connected:
-            data = client_socket.recv(constants.MAX_MSG_SIZE) # assume that we will send no message larger than this. Assume no timeout here.
-            if data == b'':
-                connected = False
-            #TODO: assume the data is a byte/bytearray representation of a class in 'messages.py' that 
-            msg = None
-            try:
-                msg = messages.deserialize(data)
-            except pickle.UnpicklingError:
-                logger.error("Unexpected Message format; could not deserialize") 
-            except EOFError:
-                logger.error("deserialization reached end of buffer without finishing; data was %d bytes, and we can only handle %d per recv call", len(data), constants.MAX_MSG_SIZE)
-                #If we're hitting this error, then we need to consider sending a length in the first few bytes and looping here until we have received that entire length
+            if flag == 0:
+                data = client_socket.recv(constants.MAX_MSG_SIZE) # assume that we will send no message larger than this. Assume no timeout here.
+                if data == b'':
+                    connected = False
+                #TODO: assume the data is a byte/bytearray representation of a class in 'messages.py' that 
+                msg = None
+                try:
+                    msg = messages.deserialize(data)
+                except pickle.UnpicklingError:
+                    logger.error("Unexpected Message format; could not deserialize") 
+                except EOFError:
+                    logger.error("deserialization reached end of buffer without finishing; data was %d bytes, and we can only handle %d per recv call", len(data), constants.MAX_MSG_SIZE)
+                    #If we're hitting this error, then we need to consider sending a length in the first few bytes and looping here until we have received that entire length
 
-            #dispatch message handler
-            if isinstance(msg, messages.ClientRequestMessage):
-                logger.critical('Received Message from client: %s', msg)
-                echo(client_socket, msg, extra_data=str(state_x))
-                state_x += 1
-                logger.info("state_x is " + str(state_x))
+                #dispatch message handler
+                if isinstance(msg, messages.ClientRequestMessage):
+                    logger.critical('Received Message from client: %s', msg)
+                    echo(client_socket, msg, extra_data=str(state_x))
+                    state_x += 1
+                    logger.info("state_x is " + str(state_x))
 
-            elif isinstance(msg, messages.LFDMessage) and msg.data == constants.MAGIC_MSG_LFD_REQUEST:
-                logger.info("Received from LFD: %s", msg.data)
-                respond_to_heartbeat(client_socket)
+                elif isinstance(msg, messages.LFDMessage) and msg.data == constants.MAGIC_MSG_LFD_REQUEST:
+                    logger.info("Received from LFD: %s", msg.data)
+                    respond_to_heartbeat(client_socket)
 
-            else: 
-                logger.info("Received unexpected message; type: [%s]", type(msg))
+                else: 
+                    logger.info("Received unexpected message; type: [%s]", type(msg))
             
         
     finally: 
@@ -93,15 +103,15 @@ def respond_to_heartbeat(client_socket, response_data=constants.MAGIC_MSG_LFD_RE
     #Require ACK?
 
 
-def passive_application_server(ip, port):
+def passive_application_server(ip, port, flag):
 
-    basic_server(application_server_handler, ip, port, logger=logger, reuse_addr=True, daemonic=True)
+    basic_server(passive_application_server_handler, flag, ip, port, logger=logger, reuse_addr=True, daemonic=True)
 
     logger.info("Echo Server Shutdown\n\n")
 
 if __name__ == "__main__":
-    ip, port = parse_args()
+    ip, port, flag = parse_args()
     DebugLogger.setup_file_handler('./app_server_' + ip+':'+str(port)+'.log', level=1)
 
-    passive_application_server(ip, port)
+    passive_application_server(ip, port, flag)
     print('done')
